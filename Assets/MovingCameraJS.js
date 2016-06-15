1d476b3abfe76760b6b1400789b572a3fe976114
Mxyz <iam_max21@hotmail.com>
2016-06-15 11:06:55 +0900

    Moving camera around box

--- Assets/MovingCameraJS.js
+++ Assets/MovingCameraJS.js
@@ -1,14 +1,14 @@
 ﻿#pragma strict
 
  var rotation : Quaternion;
- var radius = Vector3(100,0,0);
+ var radius = Vector3(1000,0,0);
  var currentRotation = 0.0;
  var rotateCamera = 0.0;
  function Update()
  {
-     currentRotation += Input.GetAxisRaw("Horizontal")*Time.deltaTime*100;
+     currentRotation += Input.GetAxisRaw("Horizontal");
      rotateCamera = Input.GetAxisRaw("Horizontal");
      rotation.eulerAngles = Vector3(0, currentRotation, 0);
      transform.position = rotation * radius;
-     transform.Rotate(0, rotateCamera, 0);
+     transform.Rotate(0, rotateCamera*1, 0);
  }
